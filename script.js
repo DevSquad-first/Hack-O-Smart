817a5488eeb401e0e2a52c6c379dc5c2f3f7da56
Pranav <pranav7651@gmail.com>
2023-05-06 00:19:54 +0530

    added some fronted

---
+++ script.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    window.location.href = "nearby.html";
+  }
+
+function bus(){
+    window.location.href = "home.html";
+}
+
+function car() {
+    window.location.href = "home.html";
+}
+
+function auto() {
+    window.location.href = "home.html";
+}
