4529f723ea0e989b33a89024ab0f3b878342f426
Pranav <pranav7651@gmail.com>
2023-05-06 16:10:22 +0530

    frontend

---
+++ nearby.js
@@ -0,0 +1,11 @@
+function bus() {
+    window.location.href = "bustop.html";
+}
+
+function pbs() {
+    window.location.href = "pbs.html";
+}
+
+function auto() {
+    window.location.href = "auto.html";
+}
---
+++ script.js
@@ -0,0 +1,28 @@
+
+function login(){
+    window.location.href = "home3.html";
+}
+
+function myFunction() {
+    window.location.href = "nearby.html";
+  }
+
+function bus(){
+    window.location.href = "home.html";
+}
+
+function car() {
+    window.location.href = "home.html";
+}
+
+function auto() {
+    window.location.href = "home.html";
+}
+
+function loginpage(){
+    window.location.href = "lhome.html";
+}
+
+function info(){
+    window.location.href = "AboutUs.html";
+}
